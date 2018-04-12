4cca4d00b2f4e62dfbe54dc9dc9a43a0fc2703bb
Evgeniy Kaparulin <evgeniy.kaparulin@kohls.com>
2018-04-12 13:10:06 -0700

    QT Widgets application

--- main.cpp
+++ main.cpp
@@ -1,37 +1,11 @@
-#include <QGuiApplication>
-#include <QQmlApplicationEngine>
-#include <QDebug>
-#include <QQmlContext>
-#include "genealogy/person.h"
-#include "genealogy/familyname.h"
-#include "genealogy/family.h"
+#include "mainwindow.h"
+#include <QApplication>
 
 int main(int argc, char *argv[])
 {
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
 
-    genealogy::Person p;
-    p.addName("Константин");
-    p.addFamilyName(genealogy::FamilyName("Капарулин"));
-    qDebug() << p.toString();
-
-    QStringList dataList;
-    dataList.append("Item 1");
-    dataList.append("Item 2");
-    dataList.append("Item 3");
-    dataList.append("Item 4");
-
-
-
-
-
-    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
-    QGuiApplication app(argc, argv);
-
-    QQmlApplicationEngine engine;
-    engine.load(QUrl(QLatin1String("qrc:/pages/main.qml")));
-
-    QQmlContext *ctxt = engine.rootContext();
-    ctxt->setContextProperty("myModel", QVariant::fromValue(dataList));
-
-    return app.exec();
+    return a.exec();
 }
